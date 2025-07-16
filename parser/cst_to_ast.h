3ee8e4fb7a55c97087d4ae4ae98ba5217c2e406c
JabelResendiz <jabelresendiz18@gmail.com>
2025-07-16 19:32:38 -0400

    fixed cst_to_ast.h

--- parser/cst_to_ast.h
+++ parser/cst_to_ast.h
@@ -17,17 +17,47 @@
 #define WHITE "\x1B[37m"
 #define RESET "\x1B[0m"
 
-ASTNode* cst_to_ast(CSTNode* cts_node);
-ASTNode*  stmtList_to_ast(CSTNode* cst_node);
-ASTNode* terminated_stmt_to_ast(CSTNode* cst_node);
+#define cst CSTNode* cst_node
 
-ASTNode* program_to_ast(CSTNode* cst_node);
-ASTNode* number_to_ast (CSTNode* cst_node);
-ASTNode* string_to_ast(CSTNode* cst_node);
-ASTNode* boolean_to_ast(CSTNode* cst_node);
-ASTNode* block_to_ast(CSTNode* cst_node);
-ASTNode* binary_expr_to_ast(CSTNode* cst_node);
+ASTNode* cst_to_ast(cst);
+ASTNode*  stmtList_to_ast(cst);
+ASTNode* terminated_stmt_to_ast(cst);
+ASTNode* stmt_to_ast(cst);
 
+// a partir de aqui son las construcciones oficiales
+ASTNode* program_to_ast(cst);
+
+// ------------ STATEMENT ----------
+ASTNode* expr_to_ast(cst);
+ASTNode* block_to_ast(cst);
+ASTNode* functionDef_to_ast(cst);
+ASTNode* typeDef_to_ast(cst);
+ASTNode* while_to_ast(cst);
+
+// el for no esta implementado no es necesario
+
+// ------------ EXPRESSION ------------
+ASTNode* conditional_to_ast(cst);
+ASTNode* let_in_to_ast(cst);
+ASTNode* assignment_to_ast(cst);
+ASTNode* funCall_to_ast(cst);
+
+// ------------ PRIMARY_EXPR ----------
+ASTNode* number_to_ast (cst);
+ASTNode* string_to_ast(cst);
+ASTNode* boolean_to_ast(cst);
+ASTNode* variable_to_ast(cst);
+
+// ------------- TYPE ----------------
+ASTNode* casting_to_ast(cst); // is y as
+ASTNode* instance_to_ast(cst);
+ASTNode* getter_to_ast(cst);
+ASTNode* setter_to_ast(cst);
+ASTNode* base_to_ast(cst);
+
+// ------------ OP --------------   
+ASTNode* binary_expr_to_ast(cst);
+ASTNode* unary_expr_to_ast(cst);
 
 
 #endif
